3f4c861adee77ffb937f98d787b3b87dff240890
Simon Accascina <simon@fotokite.com>
2021-08-13 17:27:42 +0200

    python 3.6 compatibility

--- dict2sql/test_fixtures/utils.py
+++ dict2sql/test_fixtures/utils.py
@@ -1,9 +1,23 @@
 import sqlite3
+import os
+import platform
+
+python_version = platform.python_version_tuple()
+
+if int(python_version[0]) >= 3 and int(python_version[1]) >= 7:
+
+    def open_sqlite_in_memory(filepath: str = "dict2sql/test_fixtures/chinhook.sqlite3"):
+        disk_db = sqlite3.connect(f"file:{filepath}?mode=ro", uri=True)
+        memory_db = sqlite3.connect(":memory:")
+
+        disk_db.backup(memory_db)
+        disk_db.close()
+        return memory_db
 
 
-def open_sqlite_in_memory(filepath: str = "dict2sql/test_fixtures/chinhook.sqlite3"):
-    disk_db = sqlite3.connect(f"file:{filepath}?mode=ro")
-    memory_db = sqlite3.connect(":memory:")
-    disk_db.backup(memory_db)
-    disk_db.close()
-    return memory_db
+else:
+
+    def open_sqlite_in_memory(filepath: str = "dict2sql/test_fixtures/chinhook.sqlite3"):
+        with open(filepath, "rb") as f:
+            memory_db = sqlite3.connect(database=f.read())
+        return memory_db
